b1ac138edfc7b7f36ee7edb1dd623519fda7bd81
Luis Chavez <luispazzo503@gmail.com>
2019-03-12 17:47:41 -0800

    Signing in works works, displaying appropiate error messages, must implement homepage

---
+++ index.php
@@ -0,0 +1,39 @@
+﻿<?php
+session_start();
+$loginErrors = $_SESSION['loginErrors'] ?? '';
+$_SESSION['loginErrors'] = '';
+?>
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <title>Rideshare App</title>
+</head>
+<body>
+    <div class="bg-image"></div>
+    <div class="bg-text">
+        <h2>Welcome to Rideshare!</h2>
+        <h3>Sign in:&#10</h3>
+        <?= $loginErrors ?>
+        <form name="signin" action="private/signin_handler.php" method="POST">
+            <p>
+                User name or email:
+                <input type="text" name="username">
+            </p>
+            <p>
+                Password:
+                <input type="password" name="psw">
+            </p>
+
+            <input type="submit" value="Log in">
+            <p>
+                Don't have an account?
+                <a href="public/signup.php">Sign up here</a>
+            </p>
+        </form>
+    </div>
+</body>
+</html>
---
+++ private/signin_handler.php
@@ -0,0 +1,75 @@
+<?php
+// 
+	include_once 'create_conn.php';
+	include_once 'free_res.php';
+	session_start();
+
+	// Credentials
+	$dbhost = 'localhost';
+	$dbuser = 'root';
+	$dbpass = '';
+	$dbname = 'users';
+
+	// 1. Craete a database connection
+	$conn = create_connect();
+
+	/*
+		TODO:
+		Implement Homepage.php
+
+	*/
+
+	if ($_SERVER["REQUEST_METHOD"] != "POST") {
+		header('location: error_page.php');
+		exit;
+	}
+
+	$_SESSION['loginErrors'] = '';
+
+	// Variables to hold username and password
+	$usr_name = $_POST['username'];
+	$pwd = '';
+
+	// Query which will return the password if the user name exists, then make sure user password is not null.
+	$query  = "SELECT pass FROM user_base WHERE userName = '$usr_name'";
+	$result = mysqli_query($conn, $query);
+
+	// This block makes sure the user didn't leave the inputs blank.
+	if (empty($_POST["username"])) {
+		$_SESSION['loginErrors'] .= "<p>Please enter a user name.</p>";
+	} elseif ($result ->num_rows == 0) {
+		// User name doesn't exist.
+		$_SESSION['loginErrors'] .= "<p>User name doesn't exist, please enter a "
+									. "valid user name or sign up!.</p>";
+	} 
+
+	if (empty($_POST["psw"])) {
+		// Password left blank
+		$_SESSION['loginErrors'] .= "<p>Please enter your password.</p>";
+	} elseif ($result ->num_rows == 1) {
+		// User does exist and is unique. If password is not null store password.
+		$row = mysqli_fetch_assoc($result);	// Returns the row.
+		$pwd = $row["pass"];			// Gets the hashed password.
+		if ($pwd != null) {
+			// Password exists, verify whether it matches the password inputed
+			if (password_verify($_POST["psw"], $pwd) != 1) {
+				// Password doesn't matches
+				$_SESSION['loginErrors'] .= "<p>Incorrect password, please try again.</p>";
+			}
+		} else {
+			$_SESSION['loginErrors'] .= "<p>Password is not set.</p>";
+		}
+	}
+
+	// Check whether there were any errors, redirect to homepage if not.
+
+	if ($_SESSION['loginErrors']) {
+		$redirPage = '../index.php';
+	} else {
+		//$redirPage = 'home.php';
+		die('login was successful, redir to home/admin/dashboard...');
+	}
+
+
+	header("location: $redirPage");
+?>
